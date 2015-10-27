896aee1a3c93959df1b73dcf0def5bc9c0a2b601
YiselRosario <yinarosario@gmail.com>
2015-10-27 09:42:08 -0700

    Styling fourth commit

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -2,7 +2,20 @@
 	'use strict';
 	angular.module('app', ['ui.router','ngMaterial'])
 	.config(Config);
-	function Config($stateProvider, $urlRouterProvider) {
+	function Config($stateProvider, $urlRouterProvider, $mdThemingProvider) {
+		$mdThemingProvider.theme('default')
+		.primaryPalette('amber', {
+			'default': '400', // by default use shade 400 from the pink palette for primary intentions
+			'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+			'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+			'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+		})
+		// If you specify less than all of the keys, it will inherit from the
+		// default shades
+		.accentPalette('teal', {
+			'default': '200' // use shade 200 for default, and keep all other shades the same
+		});
+
 		$stateProvider.state('Home',{
 			url: '/',
 			templateUrl: 'views/home.html'
